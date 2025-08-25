e1cedfd7bfe8b8ca111931026fad4af02be162d3
Ridhwaan <ridhwaan.jogiat@gmail.com>
2025-08-25 14:54:22 +0200

    Update environment and API URL

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Customer } from '../models/customer.model';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CustomerService {
-  private apiUrl = 'http://localhost:5258/api/customers'; 
+  private apiUrl = environment.apiUrl; 
 
   constructor(private http: HttpClient) { }
 
